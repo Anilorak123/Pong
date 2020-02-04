cd2ad3b54d4cd239c90e7c43c638911216edb15e
Anilorak123 <karolinakuszyk@gmail.com>
2020-02-04 18:28:28 +0100

    Zmiana na język polski

--- Kamień, papier, nożyce.py
+++ Kamień, papier, nożyce.py
@@ -5,20 +5,20 @@ print("Wybierz: papier, kamień lub nożyce")
 
 while True:
     game_dict = {'kamień':1, 'nożyce': 2, 'papier': 3}
-    game = "y"
-    while game == "y":
+    game = "t"
+    while game == "t":
         player_a = str(input("Gracz A: "))
         player_b = str(input("Gracz B: "))
         a = game_dict.get(player_a)
         b = game_dict.get(player_b)
         sub = a - b
         if sub == 0:
-            print("It is a draw")
+            print("Remis")
         elif sub == -1 or 2:
-            print("Player A wins")
+            print("Gracz A wygrywa")
         elif sub == -2 or 1:
-            print("Player B wins")
-        print("If you want to play again write y. If you want to finish write anything else.")
+            print("Gracz B wygrywa")
+        print("Jeśli chcesz zagrać jeszcze raz napisz t. Jeśli chcesz zakończyć napisz n")
         game = input(str())
     else:
         sys.exit()
